9bd9c5557d5bd307e0d112e162361743776db18c
Laurent Boileau <lboileau@spiria.com>
2019-04-04 12:19:06 -0400

    Remove unused config keys

--- config.js
+++ config.js
@@ -3,7 +3,5 @@ module.exports = {
   'PORT': 8009,
   'EXPRESS_SESSION_SECRET': 'djkdjbdkdbkdjbjk',
   'TWITTER_CONSUMER_KEY': '8bKm7x1kEgeYDLyzdgLw',
-  'TWITTER_CONSUMER_SECRET': 'wcetbJFxNSb2IRjq4zcqJeUA058LhTUO5hgR5g',
-  'GOOGLE_APP_ID': 'your-app-id-here',
-  'GOOGLE_CONSUMER_SECRET': 'your-consumer-secret-here',
+  'TWITTER_CONSUMER_SECRET': 'wcetbJFxNSb2IRjq4zcqJeUA058LhTUO5hgR5g'
 };
